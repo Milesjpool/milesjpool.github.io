7612cec9a1b8bf6cafafee33e4e4dca71368b2ae
Miles Pool <MilesJPool@gmail.com>
2015-06-03 03:25:52 +0100

    Restyling.

---
+++ javascript/home-script.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+
+    $('.card').hover(function () {
+        $(this).find(".definition").removeClass("hidden");
+        $(this).addClass("no-bg");
+    }, function () {
+        $(this).find(".definition").addClass("hidden");
+        $(this).removeClass("no-bg");
+    });
+
+});
