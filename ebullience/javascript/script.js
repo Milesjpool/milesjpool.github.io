e72bd3a9673884b2e0c32d2653859511202f3285
MilesJPool <MilesJPool@gmail.com>
2015-11-09 18:31:57 +0000

    Refactored getday

--- ebullience/javascript/script.js
+++ ebullience/javascript/script.js
@@ -1,10 +1,15 @@
 ﻿$(document).ready(function () {
-	var today = GetDayName((new Date()).getDay());
+	var surveyDate = GetSurveyDate();
+	var surveyDay = GetDayName(surveyDate.getDay());
 
 
 	var hour = new Date().getUTCHours()
-	$('h3').append("How was your "+today+"?");
+	$('h3').append("How was your "+surveyDay+"?");
 });
+
+function GetSurveyDate(){
+	return (new Date());
+}
 
 function GetDayName(dayOfWeek){
 	return ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][dayOfWeek];
