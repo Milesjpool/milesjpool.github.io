ab53d4cf41290302ce8983f7aa3a5c4d12010d95
MilesJPool <MilesJPool@gmail.com>
2015-11-09 18:20:11 +0000

    Added faces and question.

--- ebullience/javascript/script.js
+++ ebullience/javascript/script.js
@@ -1,3 +1,11 @@
 ﻿$(document).ready(function () {
+	var today = GetDayName((new Date()).getDay());
 
+
+	var hour = new Date().getUTCHours()
+	$('h3').append("How was your "+today+"?");
 });
+
+function GetDayName(dayOfWeek){
+	return ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][dayOfWeek];
+}
