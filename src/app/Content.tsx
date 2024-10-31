1657dcd086ce41df010f38d3fd87f81d0d8926d7
Miles <milesjpool@gmail.com>
2024-10-31 18:12:14 +0000

    begin introducing carousel.

--- src/app/Content.tsx
+++ src/app/Content.tsx
@@ -1,10 +1,42 @@
+import { useState } from "react";
 import "./Content.css";
 
 export function Content() {
   return (
     <div className='content'>
-        <img src="/mjp-comic.png" alt="MJP logo" />
-        This site is under construction
+        <Carousel />
+    </div>
+  );
+}
+
+function Carousel() {
+  const [ index, setIndex ] = useState(0);
+  return (
+    <div className='carousel'>
+      <button 
+        className='carousel-button'
+        onClick={() => setIndex(index - 1)}
+      >&lt;</button>
+      <CarouselItems />
+      <button 
+        className='carousel-button'
+        onClick={() => setIndex(index + 1)}>&gt;</button>
+    </div>
+  );
+}
+
+function CarouselItems() {
+  return (
+    <CarouselItem >      
+      <img src="/mjp-comic.png" alt="MJP logo" />
+    </CarouselItem>
+  );
+}
+
+function CarouselItem({ children }: { children: React.ReactNode }) {
+  return (
+    <div className='carousel-item'>
+      {children}
     </div>
   );
 }
