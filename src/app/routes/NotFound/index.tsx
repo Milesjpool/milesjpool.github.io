5ea7a7ee71e93122fd0987b55967e91f8f0627fd
Miles <milesjpool@gmail.com>
2024-11-11 20:04:12 +0000

    Implement 404 page

--- src/app/routes/NotFound/index.tsx
+++ src/app/routes/NotFound/index.tsx
@@ -1,9 +1,50 @@
+import { useEffect, useState } from "react";
 import "./NotFound.css";
+import clsx from "clsx";
+
+const targetValue = 404;
 
 export function NotFound() {
-  return <div className="page grow">
-    <div className="not-found">
-      Not found
+  const [value, setValue] = useAnimatedValue(100);
+  useEffect(
+    () => {
+      setValue(targetValue);
+    },
+    [targetValue, setValue]
+  );
+
+  return <div className="not-found page grow flex-col">
+    <h1 className="code">{value}</h1>
+
+    <div className={clsx("flex", "detail",
+      { hide: value !== targetValue }
+    )}>
+      🤷 (Not found)
     </div>
   </div>;
+}
+
+function useAnimatedValue(initialValue: number): [number, React.Dispatch<React.SetStateAction<number>>] {
+  const [targetValue, setTargetValue] = useState(initialValue);
+  const [currentValue, setCurrentValue] = useState(initialValue);
+
+  useEffect(() => {
+    const distance = Math.abs(currentValue - targetValue);
+    if (distance > 0) {
+      const delay = 500 / Math.pow(distance, 1.5);
+      const interval = setInterval(() => {
+        setCurrentValue((prev: any) => {
+          if (prev >= targetValue) {
+            clearInterval(interval);
+            return targetValue;
+          }
+          return prev + 1;
+        });
+      }, delay);
+
+      return () => clearInterval(interval);
+    }
+  }, [currentValue, targetValue, setCurrentValue]);
+
+  return [currentValue, setTargetValue];
 }
