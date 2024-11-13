727d43e0c924d45a08722ee80df5385e91c36dd7
Miles <milesjpool@gmail.com>
2024-11-13 02:22:41 +0000

    Update test.

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -2,9 +2,9 @@ import { render, screen } from '@testing-library/react';
 import { App } from './App';
 
 describe("App", () => {
-  it('renders MilesJPool in the document', () => {
+  it('renders welcome message in the document', () => {
     render(<App />);
-    const linkElement = screen.getByText(/MilesJPool/i);
+    const linkElement = screen.getByText(/welcome/i);
     expect(linkElement).toBeInTheDocument();
   });
 })
