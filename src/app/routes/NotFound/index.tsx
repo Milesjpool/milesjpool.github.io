8e9d4364e6e59977ae15585719c7365088c0274b
Miles <milesjpool@gmail.com>
2024-11-15 23:02:11 +0000

    Improve text-scaling on mobile devices.

--- src/app/routes/NotFound/index.tsx
+++ src/app/routes/NotFound/index.tsx
@@ -13,7 +13,7 @@ export function NotFound() {
     [setValue]
   );
 
-  return <div className="not-found page grow flex-col">
+  return <div className="not-found grow flex-col">
     <h1 className="code">{value}</h1>
 
     <div className={clsx("flex", "detail",
