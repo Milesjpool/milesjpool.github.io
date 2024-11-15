a8ffe29cdcb9c09fdfabd1018340bc6a32968c17
Miles <milesjpool@gmail.com>
2024-11-15 22:59:26 +0000

    Fix routing edge-case page layout.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,8 +15,18 @@ export function App() {
   return (
     <Router>
       <div className="app flex-col bg-primary">
+        <div className="flex grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/oh-you" >
               {/* TODO: add oh-you index page */}
               <Route index element={<Navigate to="./1" />} />
+              <Route path=":comicId" element={<OhYou />} />
+            </Route>
+            <Route path="/404" element={<NotFound />} />
+            <Route path="*" element={<Navigate to="/404" />} />
+          </Routes>
+        </div>
         <Footer />
       </div>
     </Router>
