7feed53ad967a30aaad472f9c09479721305ba00
Miles <milesjpool@gmail.com>
2024-11-16 01:53:37 +0000

    Use callback for inline function.

--- src/app/hooks/useScaledContentWidth.tsx
+++ src/app/hooks/useScaledContentWidth.tsx
@@ -1,17 +1,17 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useCallback } from "react";
 
 export function useScaledContentWidth(setScale: any) {
   const containerRef = useRef<HTMLDivElement>(null);
   const contentRef = useRef<HTMLIFrameElement>(null);
 
-  const scaleIframe = () => {
+  const scaleIframe = useCallback(() => {
     if (containerRef.current && contentRef.current) {
       const containerWidth = containerRef.current.offsetWidth;
       const iframeWidth = contentRef.current.offsetWidth;
       const scale = containerWidth / iframeWidth;
       setScale(scale);
     }
-  };
+  }, [setScale, containerRef, contentRef]);
 
   useEffect(() => {
     scaleIframe();
