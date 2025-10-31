11d41fb1ba69b2b916ec17c33a322872fe374eb3
Miles <milesjpool@gmail.com>
2025-10-31 02:19:10 +0000

    Make gallery items dynamic.

--- src/app/routes/Home/CarouselItems/MasonryDemo.tsx
+++ src/app/routes/Home/CarouselItems/MasonryDemo.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useMemo, useState } from "react";
+import { memo, PropsWithChildren, useCallback, useEffect, useMemo, useReducer, useState } from "react";
 import { Link } from "react-router-dom";
 import { useElementSize } from "app/hooks/useElementSize";
 import { gaussianRandom } from "ts/gaussianRandom";
@@ -16,24 +16,27 @@ export function MasonryDemo() {
     setIsHovered(false);
   };
 
+  const topRows = Math.floor(rowCount / 3);
+  const bottomRows = rowCount - (topRows + 3);
   return (
     <Link ref={divRef} to="/gallery/demo" className="masonry-demo" onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
-      <BasicRow key={`row-1`} />
-      <BasicRow key={`row-2`} />
-      <ContentRow key={`row-3`} childPosition={{ offset: 0.3, width: 6 }} >
+      {[...Array(topRows)].map((_, i) => (
+        <BasicRow key={`top-row-${i}`} />
+      ))}
+      <ContentRow key={`content-row-1`} childPosition={{ offset: 0.3, width: 6 }} >
         <h1>{"Masonry"}</h1>
       </ContentRow>
-      <ContentRow key={`row-4`} childPosition={{ offset: 0.4, width: 5 }} >
+      <ContentRow key={`content-row-2`} childPosition={{ offset: 0.4, width: 5 }} >
         <h1>{"Gallery"}</h1>
       </ContentRow>
-      <ContentRow key={`row-5`} childPosition={{ offset: 0.25, width: 12 }} >
+      <ContentRow key={`content-row-3`} childPosition={{ offset: 0.25, width: 12 }} >
         <div className="flex-row">
           <h1>{isHovered ? "📸" : "📷"}</h1>
           <p>{"A toybox 'masonry' style gallery component, built for my personal photography page."}</p>
         </div>
       </ContentRow>
-      {[...Array(rowCount - 5)].map((_, i) => (
-        <BasicRow key={`row-${i + 5}`} />
+      {[...Array(bottomRows)].map((_, i) => (
+        <BasicRow key={`bottom-row-${i}`} />
       ))}
     </Link>
   );
@@ -48,23 +51,22 @@ function BasicRow({ key }: BasicRowProps) {
   const sliceCount = Math.floor(rowSize.width / 45);
   const sliceWidth = rowSize.width / sliceCount;
 
-  const items = useMemo(() => distributeSlices(sliceCount), [sliceCount]);
+  const delay = useMemo(() => Math.random() * 3000 + 3000, []);
+  const { animationKey, items } = useItemsRedistributionInterval({ sliceCount, pad: 'around' }, delay);
 
-  return <div ref={rowRef} className="masonry-demo-row" key={key}>
+  return <div ref={rowRef} className="masonry-demo-row" key={`${key}`}>
     {items.map((length, index) => (
-      <div className="masonry-demo-item placeholder" key={`item-${index}`}
-        style={{
-          width: `calc(${length * sliceWidth}px - 0.5em)`,
-        }}
-      />
+      <PlaceholderItem key={`${animationKey}-item-${index}`} width={length * sliceWidth} />
     ))}
   </div>;
 }
 
 type ContentRowProps = PropsWithChildren<BasicRowProps & {
   childPosition: { offset: number, width: number }
+  log?: boolean
 }>
-function ContentRow({ key, children, childPosition }: ContentRowProps) {
+
+function ContentRow({ log, children, childPosition }: ContentRowProps) {
   const { elementRef: rowRef, elementSize: rowSize } = useElementSize<HTMLDivElement>();
 
   const sliceCount = Math.floor(rowSize.width / 45);
@@ -73,37 +75,81 @@ function ContentRow({ key, children, childPosition }: ContentRowProps) {
   const childOffset = Math.floor(childPosition.offset * sliceCount)
   const childWidth = Math.min(childPosition.width, sliceCount - (childOffset + 2));
 
-  const preItems = useMemo(() => distributeSlices(childOffset), [childOffset]);
-  const postItems = useMemo(() => distributeSlices(sliceCount - (childOffset + childWidth)), [childOffset, childWidth, sliceCount]);
+  const delay = useMemo(() => Math.random() * 3000 + 3000, []);
+  const { animationKey: preAnimationKey, items: preItems }
+    = useItemsRedistributionInterval({ sliceCount: childOffset, pad: 'left' }, delay);
+  const { animationKey: postAnimationKey, items: postItems }
+    = useItemsRedistributionInterval({ sliceCount: sliceCount - (childOffset + childWidth), pad: 'right' }, delay);
 
-  return <div ref={rowRef} className="masonry-demo-row" key={key}>
+  return <div ref={rowRef} className="masonry-demo-row">
     {preItems.map((length, index) => (
-      <div className="masonry-demo-item placeholder" key={`pre-item-${index}`}
-        style={{
-          width: `calc(${length * sliceWidth}px - 0.5em)`,
-        }}
-      />
+      <PlaceholderItem key={`pre-${preAnimationKey}-item-${index}`} width={length * sliceWidth} />
     ))}
-    <div className="masonry-demo-item content" key={`content-item`}
+    <div className="masonry-demo-item content" key={`content-item-${preAnimationKey}-${postAnimationKey}`}
       style={{
         width: `calc(${childWidth * sliceWidth}px - 0.5em)`,
+        transition: `width 5s ease-in-out`,
       }}
     >
       {children}
     </div>
     {
       postItems.map((length, index) => (
-        <div className="masonry-demo-item placeholder" key={`post-item-${index}`}
-          style={{
-            width: `calc(${length * sliceWidth}px - 0.5em)`,
-          }}
-        />
+        <PlaceholderItem key={`post-${postAnimationKey}-item-${index}`} width={length * sliceWidth} />
       ))
     }
   </div >;
 }
 
-function distributeSlices(sliceCount: number, { minLength, maxLength } = { minLength: 2, maxLength: 6 }): number[] {
+const PlaceholderItem = memo(({ width }: { width: number }) => {
+  return <div className="masonry-demo-item placeholder"
+    style={{
+      width: width > 0 ? `calc(${width}px - 0.5em)` : '0px',
+      margin: '0 0.25em',
+      transition: `width 5s ease-in-out`,
+    }} />;
+});
+
+type PadType = 'around' | 'left' | 'right';
+
+type UseItemsRedistributionIntervalProps = {
+  sliceCount: number
+  pad?: PadType
+}
+
+function useItemsRedistributionInterval({ sliceCount, pad }: UseItemsRedistributionIntervalProps, delay: number): { animationKey: number, items: number[] } {
+  const [animationKey, restartAnimation] = useReducer((prev: number) => prev + 1, 0);
+  useEffect(() => {
+    restartAnimation();
+  }, [sliceCount]);
+
+  const [items, setItems] = useState<number[]>(distributeSlices(sliceCount, { pad }));
+
+  const redistributeItems = useCallback(() => {
+    setItems(distributeSlices(sliceCount, { pad }));
+  }, [sliceCount, pad]);
+
+  useEffect(() => {
+    redistributeItems()
+    const timeoutId = setTimeout(() => redistributeItems(), 0);
+    return () => clearTimeout(timeoutId);
+  }, [redistributeItems]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => redistributeItems(), delay);
+    return () => clearInterval(intervalId);
+  }, [redistributeItems, delay]);
+
+  return { animationKey, items };
+}
+
+type DistributeSlicesProps = {
+  pad?: PadType
+  minLength?: number
+  maxLength?: number
+}
+
+function distributeSlices(sliceCount: number, { pad = 'around', minLength = 3, maxLength = 5 }: DistributeSlicesProps = {}): number[] {
   const items: number[] = [];
   let remainingSlices = sliceCount;
 
@@ -116,5 +162,9 @@ function distributeSlices(sliceCount: number, { minLength, maxLength } = { minLe
     items.push(itemLength);
     remainingSlices -= itemLength;
   }
-  return items;
+  const emptyItems = Math.max(0, Math.ceil((sliceCount) / minLength) - items.length);
+  const leftPad = pad === 'left' ? emptyItems : pad === 'around' ? Math.ceil(emptyItems / 2) : 0;
+  const rightPad = pad === 'right' ? emptyItems : pad === 'around' ? Math.floor(emptyItems / 2) : 0;
+
+  return [...Array(leftPad).fill(0), ...(pad === 'left' ? items.reverse() : items), ...Array(rightPad).fill(0)];
 }
